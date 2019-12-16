1a34ea4f63fda0980b8d07ac89bd69e6a5e69145
Rajneesh Kumar (Contractor) <Rajneesh.Kumar@wnco.com>
2019-12-16 08:29:58 -0600

    Updated the Editor.java file as per SonarQube checks

--- src/main/java/com/swacorp/crew/genericwrappers/editor/Editor.java
+++ src/main/java/com/swacorp/crew/genericwrappers/editor/Editor.java
@@ -1,32 +1,28 @@
 package com.swacorp.crew.genericwrappers.editor;
 
 import com.hp.lft.sdk.GeneralLeanFtException;
-import com.hp.lft.sdk.TestObject;
-import com.hp.lft.sdk.winforms.*;
-import com.hp.lft.sdk.winforms.EditField;
 import org.apache.log4j.Logger;
 
-//public class Editor<T extends TestObject>  {
 public class Editor  {
-    private final Logger LOGGER = Logger.getLogger(Editor.class);
-    private String ObjectType = "Type of object: ";
-    private String DataSuccessfullySet = "Data is successfully set into: ";
-    private String ErrorWhileSettingData = "Error occured while setting data ";
+    private final Logger logger = Logger.getLogger(Editor.class);
+    private String objectType = "Type of object: ";
+    private String dataSuccessfullySet = "Data is successfully set into: ";
+    private String errorWhileSettingData = "Error occured while setting data ";
     public <T extends com.hp.lft.sdk.winforms.EditField> void  setTextInEditBox(T obj, String data) throws GeneralLeanFtException {
         try {
             obj.setText(data);
-            LOGGER.info(DataSuccessfullySet+obj.getObjectName()+ObjectType+obj.getClass());
+            logger.info(dataSuccessfullySet +obj.getObjectName()+ objectType +obj.getClass());
         }catch(Exception e){
-            LOGGER.error(ErrorWhileSettingData+obj.getObjectName()+ObjectType+obj.getClass(), e);
+            logger.error(errorWhileSettingData +obj.getObjectName()+ objectType +obj.getClass(), e);
         }
     }
 
     public <T extends com.hp.lft.sdk.java.Editor> void  setTextInEditBox(T obj, String data) throws GeneralLeanFtException {
         try {
             obj.setText(data);
-            LOGGER.info(DataSuccessfullySet+obj.getObjectName()+ObjectType+obj.getClass());
+            logger.info(dataSuccessfullySet +obj.getObjectName()+ objectType +obj.getClass());
         }catch(Exception e){
-            LOGGER.error(ErrorWhileSettingData+obj.getObjectName()+ObjectType+obj.getClass(), e);
+            logger.error(errorWhileSettingData +obj.getObjectName()+ objectType +obj.getClass(), e);
         }
     }
 
