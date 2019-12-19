7780bf409e22bea4d7792871c5e734ed02b5a131
Rajneesh Kumar (Contractor) <Rajneesh.Kumar@wnco.com>
2019-12-18 22:39:06 -0600

    Added hashMap in Constant.java this will store all the runtime variables during execution. + Added comment to Constants.java interface

--- src/main/java/com/swacorp/crew/pages/common/Constant.java
+++ src/main/java/com/swacorp/crew/pages/common/Constant.java
@@ -1,5 +1,15 @@
 package com.swacorp.crew.pages.common;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Keep all the static variables here. these variables are not final.
+All the global variables and flas are declared here.
+*/
+
 public  class Constant {
     public static boolean INSTIALIZED = false;
+    Map<String, Object> map =new  HashMap<String, Object>();
 }
--- src/main/java/com/swacorp/crew/pages/common/Constants.java
+++ src/main/java/com/swacorp/crew/pages/common/Constants.java
@@ -1,5 +1,9 @@
 package com.swacorp.crew.pages.common;
 
+/*
+Keep all the constants here.
+All the variable declared here is of type public static final.
+*/
 public interface Constants {
     String url="www";
 }
