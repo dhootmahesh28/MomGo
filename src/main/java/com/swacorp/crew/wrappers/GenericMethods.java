c07ca31354b75bd86dea5aeaa28227f6cdaaf560
Rajneesh Kumar (Contractor) <Rajneesh.Kumar@wnco.com>
2020-01-22 14:52:39 -0600

    refactoring as per Sonar messages

--- src/main/java/com/swacorp/crew/wrappers/GenericMethods.java
+++ src/main/java/com/swacorp/crew/wrappers/GenericMethods.java
@@ -5,33 +5,21 @@ import com.hp.lft.sdk.TestObject;
 import org.apache.log4j.Logger;
 import com.swacorp.crew.interfaces.IGenericMethods;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-import static com.swacorp.crew.pages.common.CommonFormats.MONTH_DAY_YEAR;
 
 public interface  GenericMethods extends IGenericMethods {
 
-    public final static Logger LOGGER = Logger.getLogger(GenericMethods.class);
+    public static final Logger log = Logger.getLogger(GenericMethods.class);
 
     @Override
     default boolean VerifyObjectExist(TestObject o, boolean existance) {
         boolean objExist;
         try {
-            /*if (o.exists()) {
-                LOGGER.info("Object exists on application, "+o.getDisplayName());
-                objExist = true;
-            }else{
-                objExist = false;
-            }*/
-
             objExist = o.exists() ? true: false;
 
             return (objExist && existance) ? true : false;
-
         }
         catch (Exception e){
-            LOGGER.error("Object does not exist: "+o.getDisplayName());
+            log.error("Object does not exist: "+o.getDisplayName());
         }
         return false;
     }
@@ -69,11 +57,11 @@ public interface  GenericMethods extends IGenericMethods {
     default boolean Highlight(TestObject o) {
         try {
                 o.highlight();
-                LOGGER.info("Object highlighted, "+o.getDisplayName());
+                log.info("Object highlighted, "+o.getDisplayName());
                 return true;
         }
         catch (Exception e){
-            LOGGER.error("Object does not exist: "+o.getDisplayName());
+            log.error("Object does not exist: "+o.getDisplayName());
             //e.printStackTrace();
             return false;
         }
