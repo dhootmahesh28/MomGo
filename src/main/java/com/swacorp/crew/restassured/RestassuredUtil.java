3e98e82424ac63729dba9d9ebf141f4bb5a135d6
Rajneesh Kumar (Contractor) <Rajneesh.Kumar@wnco.com>
2020-01-29 09:48:45 -0600

    Added dependency for rest assured and a utility file for making Rest requests.

---
+++ src/main/java/com/swacorp/crew/restassured/RestassuredUtil.java
@@ -0,0 +1,53 @@
+package com.swacorp.crew.restassured;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.json.simple.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RestassuredUtil {
+
+    RequestSpecification req;
+    private String res;
+    private int statusCode;
+
+    public String getResponseBody(){
+        try {
+            return res;
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public int ExecuteREST(String method, String url, HashMap<String, String> heareds){
+        System.out.println("Calling ExecuteGET");
+        String temp = "";
+        String completeUri = "";
+        int i=0;
+
+        for (Map.Entry<String, String> entry : heareds.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            if (i<1) {
+                temp = temp + key + "=" + value;
+                ++i;
+            }else{
+                temp = temp + "&"+key + "=" + value;
+            }
+        }
+        completeUri = url+"?"+temp;
+        Response response = null;
+        try {
+            response = RestAssured.given()
+                    .when()
+                    .get(completeUri);
+            } catch (Exception e) {
+            e.printStackTrace();
+        }
+        res= response.asString();
+        return statusCode;
+    }
+}
---
+++ src/test/java/com/swacorp/crew/tests.regression/oqs/crew14477/TC_01_RestAssuredServiceCallDemo.java
@@ -0,0 +1,52 @@
+package com.swacorp.crew.tests.regression.oqs.crew14477;
+
+import com.hp.lft.sdk.GeneralLeanFtException;
+import com.swacorp.crew.dataprovider.TestDataProvider;
+import com.swacorp.crew.restassured.RestassuredUtil;
+import com.swacorp.crew.utils.TestManager;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.apache.log4j.Logger;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * Created by x257093 on 03-Jan-2020.
+ */
+
+// TC181510_181551_181552_
+public class TC_01_RestAssuredServiceCallDemo extends TestManager {
+    private final Logger LOGGER = Logger.getLogger(TC_01_RestAssuredServiceCallDemo.class);
+
+    @Test(priority = 6, groups = {"111"})
+    public void TC_01_RestAssuredServiceCallDemo_01() throws GeneralLeanFtException, Exception {
+
+        setScenarioName("TC_01_RestAssuredServiceCallDemo_01");
+        RestassuredUtil restUtl = new RestassuredUtil();
+
+        String baseUrl = "http://pilottrainingscheduling-qa1.swab2bqa.com";
+        HashMap<String, String> heards = new HashMap<String, String>();
+        heards.put("Content-Type","application/json");
+        heards.put("key","AvRunVGAUSGQ58CAG38S24tFZaLLFc8FFGzBNqsVk5eAcamVCPtuKutnKrd2Mr2rLAFFEbnVA8GTn9PPt4ha6Afq5Xta6s3M8tFXeNUMrpAEz3dJ6g2UYCLnQe362VS3");
+        heards.put("action","listSingle");
+        heards.put("empNumber","48109");
+
+        restUtl.ExecuteREST("GET", "http://pilottrainingscheduling-qa1.swab2bqa.com/API/tdfEmployees.php", heards );
+        System.out.print("Response body is "+restUtl.getResponseBody());
+
+
+         baseUrl = "http://pilottrainingscheduling-qa1.swab2bqa.com";
+        heards = new HashMap<String, String>();
+        heards.put("Content-Type","application/json");
+        heards.put("key","AvRunVGAUSGQ58CAG38S24tFZaLLFc8FFGzBNqsVk5eAcamVCPtuKutnKrd2Mr2rLAFFEbnVA8GTn9PPt4ha6Afq5Xta6s3M8tFXeNUMrpAEz3dJ6g2UYCLnQe362VS3");
+        heards.put("action","listSingle");
+        heards.put("empNumber","88147");
+
+        restUtl.ExecuteREST("GET", "http://pilottrainingscheduling-qa1.swab2bqa.com/API/tdfEmployees.php", heards );
+        System.out.print("Response body is "+restUtl.getResponseBody());
+    }
+}
